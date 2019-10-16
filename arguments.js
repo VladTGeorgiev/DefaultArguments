7ca93fa1cf8f721ffcd7ecbb736e6149b47e4111
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-10-16 01:38:57 +0100

    Working logic with single iteration, e.g. calling defaultArguments once

--- arguments.js
+++ arguments.js
@@ -2,18 +2,34 @@ function add(a, b) {
     return a + b;
 }
 
-// let string = add.toString()
+let defaultArguments = (fn, params) =>{
+    let fnString = String(fn)
+    let x = params.a || 0
+    let y = params.b || 0
+    let n = 2
+    var name = fnString.match(new RegExp('^(?:\\w+\\W+){' + --n + '}(\\w+)'));
+    var func = new Function("return " + 'function' + ' ' + name[1] + `(a = ${x}, b = ${y}) { return a + b; }`)();
+    // console.log(func)
+    return func
 
-// console.log(string)
-
-let defaultArguments = (add, { a: 4, b: 6 }) =>{
-    console.log(add.toString())
+//     const tmp  = {
+//         [name]: function(){
+//            return a + b
+//         }
+//     }
+//     const my = tmp[name]
+//     console.log(my) 
+// console.log(my.name)
 }
 
-defaultArguments()
+const add2 = defaultArguments(add, { b:9 })
 
-// function myFunction(arg) {
-//     alert(arg.var1 + ' ' + arg.var2 + ' ' + arg.var3);
-// }
+console.log(add2(10))
+console.log(add2(10, 7))
+console.log(add2())
 
-myFunction ({ var1: "Option 1", var2: "Option 2", var3: "Option 3" });
+const add3 = defaultArguments(add2, { b: 3, a: 2})
+
+console.log(add3(10))
+// console.log(add3())
+// console.log(add3(undefined, 10))
