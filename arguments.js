f97bc3175e112f87e530ce35424eab6603c10bba
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-10-16 22:23:42 +0100

    Handling NaN

--- arguments.js
+++ arguments.js
@@ -1,33 +1,35 @@
 function add(a, b) {
-    return a + b;
+    return a + b
 }
 
-let defaultArguments = (fn, params) =>{
-    let fnString = String(fn)
-    let functionArguments = fn.toString().replace(/^.*\(|\)(.|\s)*$/g, '');
+let defaultArguments = (passedInFunction, params) =>{
+    let passedInFunctionString = String(passedInFunction);
+    let functionArguments = passedInFunction.toString().replace(/^.*\(|\)(.|\s)*$/g, '');
 
-    let functionArgumentsPlaceholders = functionArguments.match(/[a-z]/gi)
+    let functionArgumentsPlaceholders = functionArguments.match(/[a-z]/gi);
 
-    const functionArgumentsObject = functionArgumentsPlaceholders.reduce((o, key) => Object.assign(o, {[key]: 0}), {});
+    const functionArgumentsObject = functionArgumentsPlaceholders.reduce((o, key) => Object.assign(o, {[key]: undefined}), {});
 
     let combinedObject = {...functionArgumentsObject, ...params}
 
-    let newFunctionArguments = []
+    let newFunctionArguments = [];
     for(let [key, value] of Object.entries(combinedObject)) {
         let z = key
         let l = value
         let kk = `${z} ${l}`
-        newFunctionArguments.push(kk)
+        newFunctionArguments.push(kk);
     }
-    let newFunctionArgumentsWithDefaultValues = newFunctionArguments.toString().replace(/\s+/g, '=')
+    let newFunctionArgumentsWithDefaultValues = newFunctionArguments.toString().replace(/\s+/g, '=');
 
-    let c = functionArgumentsPlaceholders.toString().replace(',', '+')
-// console.log(c)
+    let passedInFunctionNamePlace = 2
+    let newFunctionName = passedInFunctionString.match(new RegExp('^(?:\\w+\\W+){' + --passedInFunctionNamePlace + '}(\\w+)'));
 
-    let n = 2
-    let newFunctionName = fnString.match(new RegExp('^(?:\\w+\\W+){' + --n + '}(\\w+)'));
-    let func = new Function("return " + 'function' + ' ' + newFunctionName[1] + '(' + newFunctionArgumentsWithDefaultValues + ')' + '{ return ' + c  +'; }')();
-    return func
+    let newFunctionReturn = passedInFunctionString.match(new RegExp('(?<=return).*'));
+
+    let newFunctionReturnArguments = newFunctionReturn[0].match(new RegExp('([^;]+)'));
+
+    let newFunction = new Function("return " + 'function' + ' ' + newFunctionName[1] + '(' + newFunctionArgumentsWithDefaultValues + ')' + '{ return ' + newFunctionReturnArguments  +'; }')();
+    return newFunction
 }
 
 
@@ -36,7 +38,7 @@ const add2 = defaultArguments(add, { b:9 })
 
 console.log(add2(10)) // => 19
 console.log(add2(10, 7)) // => 17
-console.log(add2())
+console.log(add2()) // NaN
 
 const add3 = defaultArguments(add2, { b: 3, a: 2})
 
@@ -46,5 +48,5 @@ console.log(add3(undefined, 10)) // => 12
 
 const add4 = defaultArguments(add, { c: 3})
 
-console.log(add4(10))// => 10
+console.log(add4(10))// NaN
 console.log(add4(10, 10)) // => 20
